cb814f101999c8e337bc9d50e971f668fe775fef
shhun <calvin.abou-haidar@ens-lyon.fr>
2020-04-19 19:30:35 +0200

    added player class

--- lumdareman.py
+++ lumdareman.py
@@ -9,7 +9,9 @@ if not pygame.mixer: print('Warning, sound disabled')
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-FRAME_RATE = 30
+FRAME_RATE = 60
+PLAYER_MAX_LIFE = 100
+PLAYER_MAX_BOMBS = 5
 
 class LevelMap:
     def __init__(self, tmx_file):
@@ -28,13 +30,38 @@ class LevelMap:
                     tile = self.get_tile(gid)
                     surface.blit(tile, (x * self.tw, y * self.th))
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, position, velocity, image):
+        pygame.sprite.Sprite.__init__(self)
 
+        # For rendering
+        self.image = image
+        self.rect = self.image.get_rect()
+
+        # Player logic
+        self.position = position  # Vector2
+        self.velocity = velocity  # Vector2
+        self.life = PLAYER_MAX_LIFE
+        self.bombs = PLAYER_MAX_BOMBS
+
+
+    def update(self, delta_t):
+        # Check if player sprites collides with solid objects
+        # if so reset velocity, else make the player move accordingly
+        if not pygame.sprite.spritecollide(self, STATE.solid_blocks, False):
+            self.position += self.velocity * delta_t
+        else:
+            self.position.xy = 0, 0
+        # TODO: handle the case where player collides with non solid
+        # objects ?
+    
 class GameState:
     def __init__(self, screen):
         self.screen = screen
         self.running = 1
         self.clock = pygame.time.Clock()
 
+        self.solid_blocks = pygame.sprite.LayeredUpdates()
         self.sprites = pygame.sprite.LayeredUpdates()
         self.level = LevelMap('assets/test.tmx')
 
@@ -57,7 +84,7 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption('LumDareMan')
 
-    state = GameState(screen)
+    STATE = GameState(screen)
     state.loop()
 
     pygame.quit()
