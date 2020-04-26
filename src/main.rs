841ef72aeee79932333860a68082f2f80631bd7b
Lapinot <peio.borthelle@ens-lyon.fr>
2020-04-27 01:48:17 +0200

    rust ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(const_fn)]
 extern crate sdl2;
 extern crate sdl2_sys;
 
@@ -5,6 +6,8 @@ use std::path::Path;
 use std::io::{self,Read};
 use std::fs::File;
 use std::time::{Instant,Duration};
+use std::ops::Mul;
+
 
 use sdl2_sys::SDL_Rect;
 use sdl2::VideoSubsystem;
@@ -132,28 +135,146 @@ impl Map {
         }
         return Map { width, height, tiles };
     }
+
+    fn get(&self, x: i32, y: i32) -> &TILE {
+        return &self.tiles[x as usize + self.width * y as usize];
+    }
+}
+
+struct Ticker {
+    start: Instant,  // ticking start time
+    end: Instant,    // ticking end time
+}
+
+impl Ticker {
+    fn new() -> Ticker {
+        Ticker { start: Instant::now(), end: Instant::now() }
+    }
+    fn tick(&mut self, min_d: Duration) -> f32 {
+        let start = Instant::now();
+        let work_d = start.duration_since(self.end);
+        let frame_d = start.duration_since(self.start);
+
+        println!("work: {:.4}ms, frame: {:.4}ms",
+                 work_d.as_secs_f32() * 1000.,
+                 frame_d.as_secs_f32() * 1000.);
+
+        if frame_d < min_d {
+            std::thread::sleep(min_d - frame_d);
+        }
+
+        let end = Instant::now();
+        let true_d = end.duration_since(self.end);
+        self.start = start;
+        self.end = end;
+        return true_d.as_secs_f32();
+    }
+}
+
+#[derive(PartialEq,Eq,Copy,Clone)]
+enum Sign { Pos, Zer, Neg }
+
+impl Into<f32> for Sign {
+    #[inline]
+    fn into(self) -> f32 {
+        match self {
+            Sign::Pos => 1.,
+            Sign::Zer => 0.,
+            Sign::Neg => -1.,
+        }
+    }
+}
+
+impl Into<i32> for Sign {
+    #[inline]
+    fn into(self) -> i32 {
+        match self {
+            Sign::Pos => 1,
+            Sign::Zer => 0,
+            Sign::Neg => -1,
+        }
+    }
+}
+
+impl From<f32> for Sign {
+    #[inline]
+    fn from(x: f32) -> Sign {
+        if x == 0. { Sign::Zer }
+        else if x > 0. { Sign::Pos }
+        else { Sign::Neg }
+    }
+}
+
+impl From<bool> for Sign {
+    #[inline]
+    fn from(x: bool) -> Sign {
+        if x { Sign::Pos } else { Sign::Neg }
+    }
+}
+
+
+impl Mul<Sign> for Sign {
+    type Output = Sign;
+
+    #[inline]
+    fn mul(self, rhs: Sign) -> Sign {
+        match (self, rhs) {
+            (Sign::Zer, _) | (_, Sign::Zer) => { Sign::Zer },
+            (Sign::Pos, Sign::Neg) | (Sign::Neg, Sign::Pos) => { Sign::Neg },
+            (Sign::Pos, Sign::Pos) | (Sign::Neg, Sign::Neg) => { Sign::Pos },
+        }
+    }
+}
+
+impl Mul<i32> for Sign {
+    type Output = i32;
+
+    #[inline]
+    fn mul(self, rhs: i32) -> i32 {
+        match self {
+            Sign::Pos => rhs,
+            Sign::Zer => 0,
+            Sign::Neg => -rhs,
+        }
+    }
+}
+
+
+impl Mul<f32> for Sign {
+    type Output = f32;
+
+    #[inline]
+    fn mul(self, rhs: f32) -> f32 {
+        match self {
+            Sign::Pos => rhs,
+            Sign::Zer => 0.,
+            Sign::Neg => -rhs,
+        }
+    }
 }
 
 fn main() {
-    println!("Rect: {:?}, SDL_Rect: {:?}", std::mem::size_of::<Rect>(), std::mem::size_of::<sdl2_sys::SDL_Rect>());
     let sdl_ctx = sdl2::init().unwrap();
     let _image_ctx = image::init(image::InitFlag::PNG).unwrap();
     let video_ctx = sdl_ctx.video().unwrap();
 
-    let FRAME_T: Duration = Duration::from_secs_f32(1. / 60.);
+    let FRAME_T: Duration = Duration::from_secs_f32(1. / 30.);
 
     let window = video_ctx.window("demo", 800, 600).build().unwrap();
     let mut canvas = window.into_canvas().present_vsync().build().unwrap();
-    canvas.set_scale(2., 2.);
     canvas.set_draw_color(Color::RGB(0, 255, 255));
 
     let tex_creator = canvas.texture_creator();
     let TEX_TILES = tex_creator.load_texture("assets/sb1_blocks.png").unwrap();
     let TEX_CHAR = tex_creator.load_texture("assets/sb1_items.png").unwrap();
+
+    const TILE_SCALE: i32 = TILE::SIZE * 2;
+    const TILE_SCALEf: f32 = TILE_SCALE as f32;
+    const SPEED: f32 = 4. * TILE_SCALEf;
+    const SNAP: f32 = TILE_SCALEf / 4.;
+
     let CHAR_SRC = Rect::new(16, 0, 16, 16);
 
-    let SPEED = 3. * (TILE::SIZE as f32);
-    let SNAP = TILE::SIZE / 8;
 
 
 
@@ -162,101 +283,107 @@ fn main() {
 
     let mut event_pump = sdl_ctx.event_pump().unwrap();
 
-    let mut tile_dst = Rect::new(0, 0, TILE::SIZE as u32, TILE::SIZE as u32);
+    let mut tile_dst = Rect::new(0, 0, TILE_SCALE as u32, TILE_SCALE as u32);
 
-    let mut player_dst = Rect::new(0, 0, TILE::SIZE as u32, TILE::SIZE as u32);
-    let mut player_pos_i: (i32, i32) = (1, 1);
-    let mut player_pos_f: (f32, f32) = (0., 0.);
-    let mut player_in: (bool, bool) = (false, false);
-    let mut player_d: (i32, i32) = (0, 0);
-    let mut player_o: (i32, i32) = (0, 0);
+    let mut player_dst = Rect::new(0, 0, TILE_SCALE as u32, TILE_SCALE as u32);
+    let mut ppos_i: (i32, i32) = (1, 1);
+    let mut ppos_f: (f32, f32) = (0., 0.);
+    let mut pinput: (bool, bool) = (false, false);
+    let mut pori: (Sign, Sign) = (Sign::Pos, Sign::Pos);
 
-    let mut t_prev = Instant::now();
-    let mut t_start = Instant::now();
-    let mut dur = Duration::from_secs(0);
-    let mut delta_t: f32 = 0.;
+    let mut ticker = Ticker::new();
 
     'running: loop {
-        t_start = Instant::now();
-        dur = t_start.duration_since(t_prev);
-        if dur < FRAME_T { std::thread::sleep(FRAME_T - dur) }
-        delta_t = t_start.duration_since(t_prev).as_secs_f32();
-        println!("{:?}", delta_t * 1000.);
-        t_prev = t_start;
-
-        canvas.clear();
+        let delta_t = ticker.tick(FRAME_T);
 
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} => { break 'running },
-                Event::KeyDown { keycode: Some(Keycode::E), .. } => {
-                    player_o.0 = 1;
-                    player_in.0 = true;
+                Event::Quit {..} => { break 'running; },
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running;
                 }
-                Event::KeyDown { keycode: Some(Keycode::O), .. } => {
-                    player_o.1 = 1;
-                    player_in.1 = true;
+                Event::KeyDown { keycode: Some(Keycode::D), .. } => {
+                    pinput.0 = true;
+                    pori.0 = Sign::Pos;
+                }
+                Event::KeyDown { keycode: Some(Keycode::S), .. } => {
+                    pinput.1 = true;
+                    pori.1 = Sign::Pos;
                 }
                 Event::KeyDown { keycode: Some(Keycode::A), .. } => {
-                    player_o.0 = -1;
-                    player_in.0 = true;
+                    pinput.0 = true;
+                    pori.0 = Sign::Neg;
                 }
-                Event::KeyDown { keycode: Some(Keycode::Comma), .. } => {
-                    player_in.1 = true;
-                    player_o.1 = -1;
+                Event::KeyDown { keycode: Some(Keycode::W), .. } => {
+                    pinput.1 = true;
+                    pori.1 = Sign::Neg;
                 }
-                Event::KeyUp { keycode: Some(Keycode::E), .. } => {
-                    if (player_o.0 == 1) { player_in.0 = false; }
+                Event::KeyUp { keycode: Some(Keycode::D), .. } => {
+                    if (pori.0 == Sign::Pos) { pinput.0 = false; }
                 }
-                Event::KeyUp { keycode: Some(Keycode::O), .. } => {
-                    if (player_o.1 == 1) { player_in.1 = false; }
+                Event::KeyUp { keycode: Some(Keycode::S), .. } => {
+                    if (pori.1 == Sign::Pos) { pinput.1 = false; }
                 }
                 Event::KeyUp { keycode: Some(Keycode::A), .. } => {
-                    if (player_o.0 == -1) { player_in.0 = false; }
+                    if (pori.0 == Sign::Neg) { pinput.0 = false; }
                 }
-                Event::KeyUp { keycode: Some(Keycode::Comma), .. } => {
-                    if (player_o.1 == -1) { player_in.1 = false; }
+                Event::KeyUp { keycode: Some(Keycode::W), .. } => {
+                    if (pori.1 == Sign::Neg) { pinput.1 = false; }
                 }
                 _ => {}
             }
         }
 
-        let g0 = player_pos_f.0 * (player_o.0 as f32);
-        let g1 = player_pos_f.1 * (player_o.1 as f32);
+        if pinput.0 { ppos_f.0 += pori.0 * SPEED * delta_t; }
+        if pinput.1 { ppos_f.1 += pori.1 * SPEED * delta_t; }
 
-        player_d = player_o;
-        let d0 = if !player_in.0 && 0 <= g0 && g0 <  { -player_o.0 } else { player_o.0 };
-        if !player_in.0 {
-            if g0 <= 0. {
-            }
+        if pori.0 * ppos_f.0 > TILE_SCALEf / 2. {
+            ppos_f.0 -= pori.0 * TILE_SCALEf;
+            ppos_i.0 += Into::<i32>::into(pori.0);
+        };
+        if pori.1 * ppos_f.1 > TILE_SCALEf / 2. {
+            ppos_f.1 -= pori.1 * TILE_SCALEf;
+            ppos_i.1 += Into::<i32>::into(pori.1);
+        };
+
+        // quadrant of center
+        let q0 = Sign::from(ppos_f.0);
+        let q1 = Sign::from(ppos_f.1);
+
+        let c10 = q0 != Sign::Zer &&                    map.get(ppos_i.0 + Into::<i32>::into(q0), ppos_i.1                        ).is_solid();
+        let c01 = q1 !=                    Sign::Zer && map.get(ppos_i.0,                         ppos_i.1 + Into::<i32>::into(q1)).is_solid();
+        let c11 = q0 != Sign::Zer && q1 != Sign::Zer && map.get(ppos_i.0 + Into::<i32>::into(q0), ppos_i.1 + Into::<i32>::into(q1)).is_solid();
+
+        if q0 * ppos_f.0 + q1 * ppos_f.1 > 0.5 * TILE_SCALEf && c11 {
+            let a = Sign::from(q0 * ppos_f.0 - q1 * ppos_f.1);
+            let f0 = ppos_f.0;
+            let f1 = ppos_f.1;
+            /*if f32::abs(f0 - f1) < 1. {
+                ppos_f.0 = q0 * 0.5 * TILE_SCALEf;
+                ppos_f.1 = q1 * 0.5 * TILE_SCALEf;
+            } else if q0 * f0 < q1 * f1 {
+                ppos_f.0 = q0 * (0.5 * TILE_SCALEf - q1 * ppos_f.1);
+            } else {
+                ppos_f.1 = q1 * (0.5 * TILE_SCALEf - q0 * ppos_f.0);
+            }*/
+            ppos_f.0 = (f0 + q0*(0.5*TILE_SCALEf - q1*f1)) * 0.5;
+            ppos_f.1 = (f1 + q1*(0.5*TILE_SCALEf - q0*f0)) * 0.5;
         }
+        if c10 { ppos_f.0 = 0.; }
+        if c01 { ppos_f.1 = 0.; }
 
-        /*let g0 = player_pos_f.0 * (player_d.0 as f32);
-        let g1 = player_pos_f.1 * (player_d.1 as f32);
-        match (g0 >= 0., g1 >= 0.) {
-            (true,  true)  => {
-                let c10 = map.tiles[]
-            },
-            (true,  false) => {
-            },
-            (false, true)  => {
-            },
-            (false, false) => {
-            },
-        }*/
-
-        player_pos_f.0 += (player_d.0 as f32) * SPEED * delta_t;
-        player_pos_f.1 += (player_d.1 as f32) * SPEED * delta_t;
-
-        player_dst.x = (TILE::SIZE * player_pos_i.0) + (player_pos_f.0 as i32);
-        player_dst.y = (TILE::SIZE * player_pos_i.1) + (player_pos_f.1 as i32);
+        player_dst.x = (TILE_SCALE * ppos_i.0) + (ppos_f.0 as i32);
+        player_dst.y = (TILE_SCALE * ppos_i.1) + (ppos_f.1 as i32);
 
 
+        ////////////
+        // rendering
+        canvas.clear();
 
         for y in 0..map.height {
             for x in 0..map.width {
-                tile_dst.set_x(TILE::SIZE * x as i32);
-                tile_dst.set_y(TILE::SIZE * y as i32);
+                tile_dst.set_x(TILE_SCALE * x as i32);
+                tile_dst.set_y(TILE_SCALE * y as i32);
                 canvas.copy(&TEX_TILES,
                             map.tiles[x + map.width * y].src_rect(),
                             tile_dst).unwrap();
